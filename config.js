fc192d20b7fb0c020b849342dfd8be7c6a80651f
Karel Minarik <karmi@karmi.cz>
2010-11-21 00:06:35 +0100

    Initial commit -- clean Soca [https://github.com/quirkey/soca] app

---
+++ config.js
@@ -0,0 +1,15 @@
+{
+  "id": "ashtray",
+  "mapDirectories": {
+  "Jimfile": false,
+  "sass": false,
+  "css": "_attachments/css",
+  "config.js": "",
+  "templates": "_attachments/templates",
+  "images": "_attachments/images",
+  "db": "",
+  "hooks": false,
+  "js": "_attachments/js",
+  "index.html": "_attachments/index.html"
+}
+}
