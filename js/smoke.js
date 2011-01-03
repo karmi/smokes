c443ebfdf7a3385f7090d685d5482963caf5aef1
Karel Minarik <karmi@karmi.cz>
2011-01-03 15:59:13 +0100

    Code reformatting and fixing default_options extend

--- js/smoke.js
+++ js/smoke.js
@@ -18,27 +18,35 @@ Smoke.extend({
     // TODO: Ask @quirkey how to work around this maddness :)
     // Year
     Smoke.view('stats',
-      $.extend({ group_level : 1, startkey: [today.getFullYear(), {}, {}]}, default_options), function(data) {
+      $.extend(default_options,
+               { group_level : 1,
+                 startkey: [today.getFullYear(), {}, {}]}), function(data) {
         if (data.rows[0]) { stats.year  = data.rows[0].value; }
         // Month
         Smoke.view('stats',
-          $.extend({ group_level : 2, startkey: [today.getFullYear(), today.getMonth()+1, {}] }, default_options), function(data) {
+          $.extend(default_options,
+                   { group_level : 2,
+                     startkey: [today.getFullYear(), today.getMonth()+1, {}] }), function(data) {
             if (data.rows[0]) { stats.month = data.rows[0].value; }
             // Week
             Smoke.view('stats',
-              $.extend({ group_level : 3,
-                         startkey: [today.getFullYear(), today.getMonth()+1,today.getDate()],
-                         endkey:   [week_ago.getFullYear(), week_ago.getMonth()+1, week_ago.getDate()],
-                         limit:    7 },
-                         default_options), function(data) {
+              $.extend(default_options,
+                       { group_level : 3,
+                         startkey: [week_ago.getFullYear(), week_ago.getMonth()+1, week_ago.getDate()],
+                         endkey:   [today.getFullYear(), today.getMonth()+1,today.getDate()],
+                         limit:    7,
+                         descending: false }), function(data) {
                 if (data.rows[0]) { stats.week = data.rows[0].value; }
                 // Yesterday
                 Smoke.view('stats',
-                  $.extend({ group_level : 3, key: [yesterday.getFullYear(), yesterday.getMonth()+1,yesterday.getDate()] }, default_options), function(data) {
+                  $.extend(default_options,
+                           { group_level : 3,
+                             key: [yesterday.getFullYear(), yesterday.getMonth()+1,yesterday.getDate()] }), function(data) {
                     if (data.rows[0]) { stats.yesterday = data.rows[0].value; }
                     // Today
                     Smoke.view('stats',
-                      $.extend({ group_level : 3, key: [today.getFullYear(), today.getMonth()+1, today.getDate()] }, default_options), function(data) {
+                      $.extend(default_options,
+                               { group_level : 3, key: [today.getFullYear(), today.getMonth()+1, today.getDate()] }), function(data) {
                         if (data.rows[0]) { stats.today = data.rows[0].value; }
                         return callback(stats);
                     });
