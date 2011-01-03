e0335c2062f8b2a16bb900d0325c30b05f9d3d1f
Karel Minarik <karmi@karmi.cz>
2011-01-03 21:54:51 +0100

    Adding a bit of visual flair and cleaning up JS code in couch view, smoke.js

--- db/views/stats/map.js
+++ db/views/stats/map.js
@@ -1,4 +1,12 @@
 function(doc) {
   var d = new Date(doc.updated_at);
-  emit([d.getFullYear(), d.getMonth()+1, d.getDate()], 1);
+
+  // Let's count after midnight smokes as belonging to the previous day
+  if ( d.getHours() < 5 ) { d = new Date(doc.updated_at  - (24*60*60*1000) ); }
+
+  var year  = d.getFullYear(),
+      month = d.getMonth()+1,
+      day   = d.getDate();
+
+  emit([year, month, day], 1);
 }
--- js/smoke.js
+++ js/smoke.js
@@ -6,20 +6,24 @@ Smoke.extend({
     var stats = {};
     var default_options = {
       include_docs : false,
-      limit        : 1
+      limit        : 1,
+      descending   : true
     };
+    var d = new Date();
 
     // TODO: Ask @quirkey how to work around this maddness :)
-    Smoke.view('stats', $.extend({ group_level : 1, }, default_options), function(data) {
-      if (typeof data.rows[0] == 'undefined') return callback(stats);
-      stats.year  = data.rows[0].value;
-      Smoke.view('stats', $.extend({ group_level : 2, }, default_options), function(data) {
-        stats.month = data.rows[0].value;
-        Smoke.view('stats', $.extend({ group_level : 3, }, default_options), function(data) {
-          stats.today = data.rows[0].value;
-          callback(stats);
+    Smoke.view('stats',
+      $.extend({ group_level : 1, startkey: [d.getFullYear(), {}, {}]}, default_options), function(data) {
+        if (data.rows[0]) { stats.year  = data.rows[0].value; }
+        Smoke.view('stats',
+          $.extend({ group_level : 2, startkey: [d.getFullYear(), d.getMonth()+1, {}] }, default_options), function(data) {
+            if (data.rows[0]) { stats.month = data.rows[0].value; }
+            Smoke.view('stats',
+              $.extend({ group_level : 3, key: [d.getFullYear(), d.getMonth()+1, d.getDate()] }, default_options), function(data) {
+                if (data.rows[0]) { stats.today = data.rows[0].value; }
+                return callback(stats);
+            });
          });
-      });
     });
   }
 
