26c51e0561e1792daf468ec61c585530dec33cd3
Karel Minarik <karmi@karmi.cz>
2011-01-03 17:48:40 +0100

    [FIX] Fix extending default_options so they are not modified in place

--- js/smoke.js
+++ js/smoke.js
@@ -18,19 +18,19 @@ Smoke.extend({
     // TODO: Ask @quirkey how to work around this maddness :)
     // Year
     Smoke.view('stats',
-      $.extend(default_options,
+      $.extend({}, default_options,
                { group_level : 1,
                  startkey: [today.getFullYear(), {}, {}]}), function(data) {
         if (data.rows[0]) { stats.year  = data.rows[0].value; }
         // Month
         Smoke.view('stats',
-          $.extend(default_options,
+          $.extend({}, default_options,
                    { group_level : 2,
                      startkey: [today.getFullYear(), today.getMonth()+1, {}] }), function(data) {
             if (data.rows[0]) { stats.month = data.rows[0].value; }
             // Week
             Smoke.view('stats',
-              $.extend(default_options,
+              $.extend({}, default_options,
                        { group_level : 3,
                          startkey: [week_ago.getFullYear(), week_ago.getMonth()+1, week_ago.getDate()],
                          endkey:   [today.getFullYear(), today.getMonth()+1,today.getDate()],
@@ -42,13 +42,13 @@ Smoke.extend({
                 };
                 // Yesterday
                 Smoke.view('stats',
-                  $.extend(default_options,
+                  $.extend({}, default_options,
                            { group_level : 3,
                              key: [yesterday.getFullYear(), yesterday.getMonth()+1,yesterday.getDate()] }), function(data) {
                     if (data.rows[0]) { stats.yesterday = data.rows[0].value; }
                     // Today
                     Smoke.view('stats',
-                      $.extend(default_options,
+                      $.extend({}, default_options,
                                { group_level : 3, key: [today.getFullYear(), today.getMonth()+1, today.getDate()] }), function(data) {
                         if (data.rows[0]) { stats.today = data.rows[0].value; }
                         return callback(stats);
