233d5cda05d21ef5f1a1637bc264faf7f51f3e5e
Karel Minarik <karmi@karmi.cz>
2011-01-04 17:34:07 +0100

    Animating today's count (stepper)

--- js/app.js
+++ js/app.js
@@ -15,18 +15,25 @@
       console.log('Stats loaded:', data);
       // Render stats
       context.render('templates/stats.ejs', { stats: data }).replace('#stats');
-      // Update today count and color
-      $('#today .value')
-        .text( data.today )
-        .addClass(function() {
-          var value     = parseInt(data.today);
-          var classname = '';
-          if (value > 0) { $(this).removeClass('zero'); };
-          if (value < 6) { classname = 'lo';   };
-          if (value > 6) { classname = 'mid';  };
-          if (value > 8) { classname = 'hi';   };
-          return classname;
-        });
+      // Update today class and count (animated)
+      var today = $('#today .value');
+      today.addClass(function() {
+        var value     = parseInt(data.today);
+        var classname = '';
+        if (value > 0) { $(this).removeClass('zero'); };
+        if (value < 6) { classname = 'lo';   };
+        if (value > 6) { classname = 'mid';  };
+        if (value > 8) { classname = 'hi';   };
+        return classname;
+      });
+      (function() {
+        var original_value = parseInt(today.text()) || 0;
+        var current_value  = data.today || 0;
+        var update_value = function(value) { today.text(value); };
+        for (var i=0; i < current_value; i++) {
+          setTimeout(update_value, i*100, i);
+        };
+      })();
       // Update average count
       $('#average .value').text( data.average );
     });
