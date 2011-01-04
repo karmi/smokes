f50eb450bd6372139fc52ecdc906e2d09f2d2dd4
Karel Minarik <karmi@karmi.cz>
2011-01-04 17:05:17 +0100

    Code reformatting in app.js

--- js/app.js
+++ js/app.js
@@ -16,15 +16,17 @@
       // Render stats
       context.render('templates/stats.ejs', { stats: data }).replace('#stats');
       // Update today count and color
-      $('#today .value').text( data.today ).addClass(function() {
-        var value     = parseInt(data.today);
-        var classname = '';
-        if (value > 0) { $(this).removeClass('zero'); };
-        if (value < 6) { classname = 'lo';   };
-        if (value > 6) { classname = 'mid';  };
-        if (value > 8) { classname = 'hi';   };
-        return classname;
-      });
+      $('#today .value')
+        .text( data.today )
+        .addClass(function() {
+          var value     = parseInt(data.today);
+          var classname = '';
+          if (value > 0) { $(this).removeClass('zero'); };
+          if (value < 6) { classname = 'lo';   };
+          if (value > 6) { classname = 'mid';  };
+          if (value > 8) { classname = 'hi';   };
+          return classname;
+        });
       // Update average count
       $('#average .value').text( data.average );
     });
