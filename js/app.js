4b02f79707f432e994112f4b3c67fc676721f335
Karel Minarik <karmi@karmi.cz>
2011-01-04 10:55:43 +0100

    Added default values for today/average counts, added colorization for today's count, cleaning up JS (tmbundle warnings)

--- js/app.js
+++ js/app.js
@@ -13,20 +13,30 @@
     this.bind('stats-loaded', function(e, data) {
       var context = this;
       console.log('Stats loaded:', data);
+      // Render stats
       context.render('templates/stats.ejs', { stats: data }).replace('#stats');
-      $('#today .value').text( data.today );
+      // Update today count and color
+      $('#today .value').text( data.today ).addClass(function() {
+        var value     = parseInt(data.today);
+        var classname = '';
+        if (value > 0) { $(this).removeClass('zero'); };
+        if (value < 6) { classname = 'lo';   };
+        if (value > 6) { classname = 'mid';  };
+        if (value > 8) { classname = 'hi';   };
+        return classname;
+      });
+      // Update average count
       $('#average .value').text( data.average );
     });
 
     // -- Routes
 
     this.get('#/', function(context) {
-      Smoke.stats( function(data) { context.trigger( 'stats-loaded', data ) } );
+      Smoke.stats( function(data) { context.trigger( 'stats-loaded', data ); } );
     });
 
     this.post('#/create', function(context) {
-      console.log('CREATE')
-      Smoke.create(this.params['smoke'], function() { context.redirect('#/') });
+      Smoke.create(this.params['smoke'], function() { context.redirect('#/'); });
       ;
     });
 
