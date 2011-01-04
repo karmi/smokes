dbb3e9825faf7e3d8d869209e276a73ba876f951
Karel Minarik <karmi@karmi.cz>
2011-01-04 17:38:37 +0100

    Animating today's count CSS class

--- js/app.js
+++ js/app.js
@@ -17,21 +17,22 @@
       context.render('templates/stats.ejs', { stats: data }).replace('#stats');
       // Update today class and count (animated)
       var today = $('#today .value');
-      today.addClass(function() {
-        var value     = parseInt(data.today);
-        var classname = '';
-        if (value > 0) { $(this).removeClass('zero'); };
-        if (value < 6) { classname = 'lo';   };
-        if (value > 6) { classname = 'mid';  };
-        if (value > 8) { classname = 'hi';   };
-        return classname;
-      });
+      var count = parseInt(data.today);
+      if (count > 0) { today.removeClass('zero'); };
       (function() {
         var original_value = parseInt(today.text()) || 0;
         var current_value  = data.today || 0;
-        var update_value = function(value) { today.text(value); };
+        var update_value = function(value) { return today.text(value); };
+        var update_class = function(value) {
+          var classname = '';
+          if (value < 6) { classname = 'lo';   };
+          if (value > 6) { classname = 'mid';  };
+          if (value > 8) { classname = 'hi';   };
+          today.addClass(classname);
+        };
         for (var i=0; i < current_value; i++) {
-          setTimeout(update_value, i*100, i);
+          setTimeout(update_value, i*75, i);
+          setTimeout(update_class, i*75, i);
         };
       })();
       // Update average count
