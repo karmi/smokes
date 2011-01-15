9928f7a9b3b1032f2540b50366fdca96e20b0348
Karel Minarik <karmi@karmi.cz>
2011-01-15 20:00:07 +0100

    Added proper MIME type for cache.manifest and hook into before_build

--- hooks/before_build.rb
+++ hooks/before_build.rb
@@ -1,2 +1,7 @@
 # any pre build scripting or plugins should go here
 plugin 'jim'
+
+# Add mimetype for CACHE MANIFEST file
+# Cf. "HTML5 Offline Application Cache"
+MIME::Types.add MIME::Type.new('text/cache-manifest') { |t| t.extensions << 'manifest' }
+system "rake manifest"
